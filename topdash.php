72266e73177ade761fd52e882db69961be11f348
Urban Sanden <hej@urre.me>
2015-04-27 15:03:14 +0200

    Tested up to 4.2

--- topdash.php
+++ topdash.php
@@ -9,7 +9,7 @@
 * License: GPL2
 */
 
-/*  Copyright 2014 Urban Sanden (email: hej@urre.me)
+/*  Copyright 2015 Urban Sanden (email: hej@urre.me)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
